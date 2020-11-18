60cdab2f6f486b6450589fa2075324fe4c6fe62f
creattech <crystalsmcneil@gmail.com>
2020-11-18 17:05:26 -0600

    adds auth and main module and main component skeleton

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'movierater-angular';
-}
+export class AppComponent {}
