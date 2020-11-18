7af57a8d9acf3e33ffda6f853dc49fd5d455ce5b
creattech <crystalsmcneil@gmail.com>
2020-11-18 17:07:49 -0600

    adds auth component skeleton

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AuthComponent],
   imports: [
     CommonModule
   ]
