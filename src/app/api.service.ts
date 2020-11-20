abd73a821dc3da54cafa675c9649e1ca355a669a
creattech <crystalsmcneil@gmail.com>
2020-11-20 14:22:24 -0600

    adds Movie type to api service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {Movie} from './models/Movie';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +18,11 @@ export class ApiService {
 
   // tslint:disable-next-line:typedef
   getMovies() {
-    return this.httpClient.get(this.baseUrl, {headers: this.headers});
+    return this.httpClient.get<Movie[]>(this.baseUrl, {headers: this.headers});
   }
 
   getMovie(id: number) {
-    return this.httpClient.get(`${this.baseUrl}${id}/`, {headers: this.headers});
+    return this.httpClient.get<Movie>(`${this.baseUrl}${id}/`, {headers: this.headers});
   }
 
   rateMovie(rate: number, movieId: number) {
