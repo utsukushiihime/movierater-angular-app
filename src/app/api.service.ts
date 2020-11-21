6b82e3372895da302b1fbc4128751d74945108f9
creattech <crystalsmcneil@gmail.com>
2020-11-20 21:59:08 -0600

    code cleanup in api.service.ts

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -4,59 +4,76 @@ import { Movie } from './models/Movie';
 import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
   baseUrl = 'http://127.0.0.1:8000/';
   baseMovieUrl = `${this.baseUrl}api/movies/`;
   headers = new HttpHeaders({
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   });
 
   constructor(
     private httpClient: HttpClient,
     private cookieService: CookieService
-  ) { }
+  ) {}
 
   getMovies() {
-    return this.httpClient.get<Movie[]>(this.baseMovieUrl, {headers: this.getAuthHeaders()});
+    return this.httpClient.get<Movie[]>(this.baseMovieUrl, {
+      headers: this.getAuthHeaders(),
+    });
   }
   getMovie(id: number) {
-    return this.httpClient.get<Movie>(`${this.baseMovieUrl}${id}/`, {headers: this.getAuthHeaders()});
+    return this.httpClient.get<Movie>(`${this.baseMovieUrl}${id}/`, {
+      headers: this.getAuthHeaders(),
+    });
   }
   createMovie(title: string, description: string) {
-    const body = JSON.stringify({title, description});
-    return this.httpClient.post(`${this.baseMovieUrl}`, body, {headers: this.getAuthHeaders()});
+    const body = JSON.stringify({ title, description });
+    return this.httpClient.post(`${this.baseMovieUrl}`, body, {
+      headers: this.getAuthHeaders(),
+    });
   }
   updateMovie(id: number, title: string, description: string) {
-    const body = JSON.stringify({title, description});
-    return this.httpClient.put(`${this.baseMovieUrl}${id}/`, body, {headers: this.getAuthHeaders()});
+    const body = JSON.stringify({ title, description });
+    return this.httpClient.put(`${this.baseMovieUrl}${id}/`, body, {
+      headers: this.getAuthHeaders(),
+    });
   }
   deleteMovie(id: number) {
-    return this.httpClient.delete(`${this.baseMovieUrl}${id}/`, {headers: this.getAuthHeaders()});
+    return this.httpClient.delete(`${this.baseMovieUrl}${id}/`, {
+      headers: this.getAuthHeaders(),
+    });
   }
 
   rateMovie(rate: number, movieId: number) {
-    const body = JSON.stringify({stars: rate});
-    return this.httpClient.post(`${this.baseMovieUrl}${movieId}/rate_movie/`, body, {headers: this.getAuthHeaders()});
+    const body = JSON.stringify({ stars: rate });
+    return this.httpClient.post(
+      `${this.baseMovieUrl}${movieId}/rate_movie/`,
+      body,
+      { headers: this.getAuthHeaders() }
+    );
   }
 
   loginUser(authData) {
     const body = JSON.stringify(authData);
-    return this.httpClient.post(`${this.baseUrl}auth/`, body, {headers: this.headers});
+    return this.httpClient.post(`${this.baseUrl}auth/`, body, {
+      headers: this.headers,
+    });
   }
 
   registerUser(authData) {
     const body = JSON.stringify(authData);
-    return this.httpClient.post(`${this.baseUrl}api/users/`, body, {headers: this.headers});
+    return this.httpClient.post(`${this.baseUrl}api/users/`, body, {
+      headers: this.headers,
+    });
   }
 
   getAuthHeaders() {
     const token = this.cookieService.get('mr-token');
     return new HttpHeaders({
       'Content-Type': 'application/json',
-      Authorization: `Token ${token}`
+      Authorization: `Token ${token}`,
     });
   }
 }
