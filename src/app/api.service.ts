844fc847849401722b5bd0d0a046d0a3a2d39f40
creattech <crystalsmcneil@gmail.com>
2020-11-20 21:27:50 -0600

    updates formatting adds prettier to project

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -19,48 +19,39 @@ export class ApiService {
     private cookieService: CookieService
   ) { }
 
-  // tslint:disable-next-line:typedef
   getMovies() {
     return this.httpClient.get<Movie[]>(this.baseMovieUrl, {headers: this.getAuthHeaders()});
   }
-  // tslint:disable-next-line:typedef
   getMovie(id: number) {
     return this.httpClient.get<Movie>(`${this.baseMovieUrl}${id}/`, {headers: this.getAuthHeaders()});
   }
-  // tslint:disable-next-line:typedef
   createMovie(title: string, description: string) {
     const body = JSON.stringify({title, description});
     return this.httpClient.post(`${this.baseMovieUrl}`, body, {headers: this.getAuthHeaders()});
   }
-  // tslint:disable-next-line:typedef
   updateMovie(id: number, title: string, description: string) {
     const body = JSON.stringify({title, description});
     return this.httpClient.put(`${this.baseMovieUrl}${id}/`, body, {headers: this.getAuthHeaders()});
   }
-  // tslint:disable-next-line:typedef
   deleteMovie(id: number) {
     return this.httpClient.delete(`${this.baseMovieUrl}${id}/`, {headers: this.getAuthHeaders()});
   }
 
-  // tslint:disable-next-line:typedef
   rateMovie(rate: number, movieId: number) {
     const body = JSON.stringify({stars: rate});
     return this.httpClient.post(`${this.baseMovieUrl}${movieId}/rate_movie/`, body, {headers: this.getAuthHeaders()});
   }
 
-  // tslint:disable-next-line:typedef
   loginUser(authData) {
     const body = JSON.stringify(authData);
     return this.httpClient.post(`${this.baseUrl}auth/`, body, {headers: this.headers});
   }
 
-  // tslint:disable-next-line:typedef
   registerUser(authData) {
     const body = JSON.stringify(authData);
     return this.httpClient.post(`${this.baseUrl}api/users/`, body, {headers: this.headers});
   }
 
-  // tslint:disable-next-line:typedef
   getAuthHeaders() {
     const token = this.cookieService.get('mr-token');
     return new HttpHeaders({
